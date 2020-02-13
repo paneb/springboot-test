56ba16c912c2bf85966de03122b5df5da04d77ce
Oriol Saludes <oriol.saludes@apiumhub.com>
2020-02-13 17:53:58 +0100

    add junit and unit test

---
+++ src/test/java/com/paytah/example/demo/DemoUnitTests.java
@@ -0,0 +1,11 @@
+package com.paytah.example.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DemoUnitTests {
+    @Test
+    void shouldAsssertTrue() {
+        Assertions.assertTrue(true);
+    }
+}
